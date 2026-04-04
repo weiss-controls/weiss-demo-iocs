12d7b05eb54dfc63ddc44281e7ccc0042a8b6a37
AndreFavotto <andrefavotto@hotmail.com>
2026-04-04 16:05:08 +0200

    genericIO: add separate 3 phase system and generic IOs

--- genericIO/iocBoot/iocgenericIO/st.cmd
+++ genericIO/iocBoot/iocgenericIO/st.cmd
@@ -9,7 +9,17 @@ genericIO_registerRecordDeviceDriver pdbbase
 
 epicsEnvSet("P", "demo:")
 epicsEnvSet("R", "")
-dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R)")
+dbLoadRecords("./genericIOApp/Db/3PhaseSyst.db", "P=$(P), R=$(R)")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=01")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=02")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=03")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=04")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=05")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=06")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=07")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=08")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=09")
+dbLoadRecords("./genericIOApp/Db/genericIO.db", "P=$(P), R=$(R), IDX=10")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
